e69b44f292da6de93949128193f6b155efc3dcbf
Adrián Ramón <adrianramon2003@gmail.com>
2023-01-27 11:58:56 +0100

    Añadidas imagenes y fixes para el boton de notificaciones

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
 let boton= document.getElementById("ocultar")
+let boton2= document.getElementById("triangulo")
 
-function prueba(){
+function ocultar(){
     boton.style.display="none"
+    boton2.style.display="none"
+    window.location.replace("../public/notificaciones.html");
+
 }
